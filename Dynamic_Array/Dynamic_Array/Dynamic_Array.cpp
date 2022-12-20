eac5beb87ce9e0a1009f9678f871949b3608474d
mamedovaem <mamedova_elmira@mail.ru>
2022-12-20 22:43:38 +0300

    Dynamic Array
    Adding the whole project

---
+++ Dynamic_Array/Dynamic_Array/Dynamic_Array.cpp
@@ -0,0 +1,62 @@
+﻿// Dynamic_Array.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Array.h"
+
+int main()
+{
+    Array<int> a;
+    for (int i = 0; i < 10; ++i)
+        a.insert(i + 1);
+    for (int i = 0; i < a.size(); ++i)
+        a[i] *= 2;
+    for (auto it = a.iterator(); it.has_current(); it.next())
+        std::cout << it.get() << std::endl;
+    for (auto it = a.reverseIterator(); it.has_current(); it.next())
+    {
+        it.set(5);
+        std::cout << it.get() << std::endl;
+    }
+
+    Array<std::string> b;
+    for (char ch = 'A'; ch <= 'Z'; ++ch)
+        b.insert(std::string(1, ch));
+
+    b.insert(0, "Q");
+    b.insert(12, "8");
+    b.remove(3);
+    b.insert(b.size(), "4");
+
+    for (int i = 0; i < b.size(); ++i)
+        b[i] += (char) i;
+
+    for (auto it = b.iterator(); it.has_current(); it.next())
+        std::cout << it.get() << std::endl;
+    
+    Array<std::string> d;
+    d = b;
+
+    for (auto it = b.reverseIterator(); it.has_current(); it.next())
+    {
+        it.set("erased");
+        std::cout << it.get() << std::endl;
+    }
+
+    for (auto it = d.iterator(); it.has_current(); it.next())
+        std::cout << it.get() << std::endl;
+  
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Dynamic_Array/Tests/test.cpp
@@ -0,0 +1,277 @@
+﻿#include "pch.h"
+#include "../Dynamic_Array/Array.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(TestArrayInt, TestDefaultConstructor)
+{
+    Array<int> a;
+    EXPECT_EQ(a.size(), 0);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+
+TEST(TestArrayInt, TestCapacityConstructor)
+{
+    Array<int> a { 10 };
+    EXPECT_EQ(a.size(), 0);
+    EXPECT_EQ(a.capacity(), 10);
+}
+
+TEST(TestArrayInt, TestCopyConstructor)
+{
+    Array<int> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    Array<int> b(a);
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_EQ(a[i], b[i]);
+    }
+}
+
+TEST(TestArrayInt, TestMoveConstructor)
+{
+    Array<int> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    Array<int> b(std::move(a));
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_NO_THROW(b[i]);
+        EXPECT_EQ(b[i], i+1);
+    }
+}
+
+TEST(TestArrayInt, TestAssignmentOperator)
+{
+    Array<int> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    Array<int> b;
+    b = a;
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_EQ(a[i], b[i]);
+    }
+}
+
+TEST(TestArrayInt, TestInsert)
+{
+    Array<int> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        EXPECT_NO_THROW(a.insert(i + 1));
+    }
+
+    EXPECT_EQ(a.size(), 5);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+TEST(TestArrayInt, TestIndexedInsert)
+{
+    Array<int> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    EXPECT_NO_THROW(a.insert(0,0));
+    EXPECT_NO_THROW(a.insert(10, 0));
+    EXPECT_NO_THROW(a.insert(22, 0));
+
+    EXPECT_EQ(a.size(), 23);
+    EXPECT_EQ(a.capacity(), 32);
+}
+
+TEST(TestArrayInt, TestDoubleCapacity)
+{
+    Array<int> a;
+    for (int i = 0; i < 50; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    EXPECT_EQ(a.size(), 50);
+    EXPECT_EQ(a.capacity(), 64);
+}
+
+TEST(TestArrayInt, TestRemove)
+{
+    Array<int> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    EXPECT_NO_THROW(a.remove(0));
+
+    EXPECT_EQ(a.size(), 4);
+    EXPECT_EQ(a[0], 2);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+TEST(TestArrayInt, TestIndexOperator)
+{
+    Array<int> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        a.insert(i + 1);
+    }
+
+    EXPECT_NO_THROW(a[2] = 0);
+
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[2], 0);
+    EXPECT_EQ(a[4], 5);
+}
+
+TEST(TestArrayString, TestDefaultConstructor)
+{
+    Array<std::string> a;
+    EXPECT_EQ(a.size(), 0);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+
+TEST(TestArrayString, TestCapacityConstructor)
+{
+    Array<std::string> a { 10 };
+    EXPECT_EQ(a.size(), 0);
+    EXPECT_EQ(a.capacity(), 10);
+}
+
+TEST(TestArrayString, TestCopyConstructor)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    Array<std::string> b(a);
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_EQ(a[i], b[i]);
+    }
+}
+
+TEST(TestArrayString, TestMoveConstructor)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    Array<std::string> b(std::move(a));
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_NO_THROW(b[i]);
+        EXPECT_EQ(b[i], "akdhksbhashgkgbvzbshv" + i);
+    }
+}
+
+TEST(TestArrayString, TestAssignmentOperator)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    Array<std::string> b;
+    b = a;
+
+    for (int i = 0; i < 20; ++i)
+    {
+        EXPECT_EQ(a[i], b[i]);
+    }
+}
+
+TEST(TestArrayString, TestInsert)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        EXPECT_NO_THROW(a.insert("akdhksbhashgkgbvzbshv" + i));
+    }
+
+    EXPECT_EQ(a.size(), 5);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+TEST(TestArrayString, TestIndexedInsert)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 20; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    EXPECT_NO_THROW(a.insert(0,"akdhksbhashgkgbvzbshv" + 0));
+    EXPECT_NO_THROW(a.insert(10, "akdhksbhashgkgbvzbshv" + 0));
+    EXPECT_NO_THROW(a.insert(22,"akdhksbhashgkgbvzbshv" + 0));
+
+    EXPECT_EQ(a.size(), 23);
+    EXPECT_EQ(a.capacity(), 32);
+}
+
+TEST(TestArrayString, TestDoubleCapacity)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 50; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    EXPECT_EQ(a.size(), 50);
+    EXPECT_EQ(a.capacity(), 64);
+}
+
+TEST(TestArrayString, TestRemove)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    EXPECT_NO_THROW(a.remove(0));
+
+    EXPECT_EQ(a.size(), 4);
+    EXPECT_EQ(a[0], "akdhksbhashgkgbvzbshv" + 1);
+    EXPECT_EQ(a.capacity(), 8);
+}
+
+TEST(TestArrayString, TestIndexOperator)
+{
+    Array<std::string> a;
+    for (int i = 0; i < 5; ++i)
+    {
+        a.insert("akdhksbhashgkgbvzbshv" + i);
+    }
+
+    EXPECT_NO_THROW(a[2] = "akdhksbhashgkgbvzbshv" + 0);
+
+    EXPECT_EQ(a[0], "akdhksbhashgkgbvzbshv" + 0);
+    EXPECT_EQ(a[2], "akdhksbhashgkgbvzbshv" + 0);
+    EXPECT_EQ(a[4], "akdhksbhashgkgbvzbshv" + 4);
+}
